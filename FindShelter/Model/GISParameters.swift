5b48fd7bc11d29f79f0f2d3f7bd3134bdb403e0f
Jacob Taxén <jacob.taxen@afconsult.com>
2017-05-12 11:57:00 +0200

    Function for create request parameters

---
+++ FindShelter/Model/GISParameters.swift
@@ -0,0 +1,34 @@
+//
+//  GISParameters.swift
+//  FindShelter
+//
+//  Created by ÅF Jacob Taxén on 2017-05-12.
+//  Copyright © 2017 Jacob Taxén. All rights reserved.
+//
+
+import Foundation
+
+class GISParameters {
+
+	static let shared = GISParameters()
+	
+	private init() {}
+	
+	public func makeParameters() -> [String: AnyObject] {
+		let parameters = [
+			ParameterKeys.Contains:         ParameterValues.Contains as AnyObject,
+			ParameterKeys.Layers:           ParameterValues.Layers as AnyObject,
+			ParameterKeys.ReturnGeometry:   ParameterValues.ReturnGeometry as AnyObject,
+			ParameterKeys.F:                ParameterValues.F as AnyObject,
+			ParameterKeys.SpatialReference: ParameterValues.SpatialReference as AnyObject
+		]
+		return parameters
+	}
+	
+	public func makeParameters(search: String) -> [String: AnyObject] {
+	
+		var parameters = makeParameters()
+		parameters[ParameterKeys.SearchText] = search as AnyObject
+		return parameters
+	}
+}
---
+++ FindShelterTests/GISParametersTests.swift
@@ -0,0 +1,32 @@
+//
+//  GISParametersTests.swift
+//  FindShelter
+//
+//  Created by ÅF Jacob Taxén on 2017-05-12.
+//  Copyright © 2017 Jacob Taxén. All rights reserved.
+//
+
+import XCTest
+@testable import FindShelter
+
+class GISParametersTests: XCTestCase {
+	
+	let GISParams = GISParameters.shared
+
+	func testMakeParameters() {
+	
+		let parameters = GISParams.makeParameters()
+		
+		XCTAssertEqual(parameters[GISParameters.ParameterKeys.Contains] as! Bool, true)
+		XCTAssertEqual(parameters[GISParameters.ParameterKeys.F] as! String, "json")
+	}
+	
+	func testMakeParametersWithSearchText() {
+	
+		let searchString = "Hallenborgs gata"
+		let parameters = GISParams.makeParameters(search: searchString)
+		
+		XCTAssertEqual(parameters[GISParameters.ParameterKeys.SearchText] as! String, searchString)
+		
+	}
+}
