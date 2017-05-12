f1b78542cedbc9dc1504b6ac5d477a56469db306
Jacob Taxén <jacob.taxen@afconsult.com>
2017-05-12 15:20:49 +0200

    Added api client class

---
+++ FindShelter/Model/ArcGISClient.swift
@@ -0,0 +1,19 @@
+//
+//  ArcGISClient.swift
+//  FindShelter
+//
+//  Created by ÅF Jacob Taxén on 2017-05-12.
+//  Copyright © 2017 Jacob Taxén. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class ArcGISClient {
+
+	func makeAPIRequest(url: URL, parameters: [String: AnyObject]) {
+		
+	}
+	
+}
+
--- FindShelter/Model/GISParameters.swift
+++ FindShelter/Model/GISParameters.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class GISParameters {
+class GISParameters: Parameters {
 
 	static let shared = GISParameters()
 	
